0e305d29f3efddc6e297bc232471607865291fb2
Ketan M <km0@ornl.gov>
2018-07-02 17:08:01 -0400

    swiftify prob1

---
+++ src/runprob1.swift
@@ -0,0 +1,15 @@
+import files;
+import unix;
+
+app (file out) myawk (file awkprog, file infile){
+    "/usr/bin/awk" "-f" awkprog infile @stdout=out
+}
+
+file aminer[]=glob("/dev/shm/aminer_mag_papers/*.txt");
+file outfiles[];
+
+foreach v, i in aminer{
+    outfiles[i] = myawk(input("/home/km0/SMC18/src/prob1.awk"), v);
+}
+
+file joined <"joined.txt"> = cat(outfiles);
