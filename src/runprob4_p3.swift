61c7b12d8f60085177482057e3766458de680853
Ketan M <km0@ornl.gov>
2018-07-10 15:53:48 -0400

    changes

--- src/runprob4_p3.swift
+++ src/runprob4_p3.swift
@@ -7,11 +7,10 @@ app (file out) myawk (file awkprog, file infile, file stopwords, string yr){
     "/usr/bin/awk" "-v" yr "-f" awkprog stopwords infile @stdout=out
 }
 
-file aminer[]=glob("/dev/shm/aminer_mag_papers/aminer*.txt");
+file aminer[]=glob("/dev/shm/aminer_mag_papers/*.txt");
 
-foreach y in [2011:2015:1]{
+foreach y in [1800:2017:1]{
     file yearfiles[];
-    printf("%i\n",y);
     foreach v, i in aminer{
         yearfiles[i] = myawk(input("/home/km0/SMC18/src/prob4_p3.awk"), v, input("/home/km0/SMC18/data/stop_words.txt"), sprintf("yr=%s",toString(y)));
     }
