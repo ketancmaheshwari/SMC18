322e1d5985cb652e68880b0b5fa5f1bcd1fc9df7
Ketan Maheshwari <km0@ornl.gov>
2018-07-18 11:40:52 -0400

    minor

--- src/runprob1_p1.swift
+++ src/runprob1_p1.swift
@@ -2,14 +2,14 @@ import files;
 import unix;
 
 app (file out) myawk (file awkprog, file infile, string topic){
-    "/usr/bin/awk" "-v" "topic="topic "-f" awkprog infile @stdout=out
+    "/usr/bin/awk" "-v" topic "-f" awkprog infile @stdout=out
 }
 
 file aminer[]=glob("/dev/shm/aminer_mag_papers/*.txt");
 file outfiles[];
 
 foreach v, i in aminer{
-    outfiles[i] = myawk(input("/home/km0/SMC18/src/prob1_p1.awk"), v, "meditation");
+    outfiles[i] = myawk(input("/home/km0/SMC18/src/prob1_p1.awk"), v, "topic=meditation");
 }
 
 file joined <"joined.txt"> = cat(outfiles);
